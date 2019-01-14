26b9ce3e82c8fc4799e47410261644de6466b398
Mahdy Beygi <mahdyb@gmail.com>
2019-01-14 16:15:36 +0330

    update gitignore, fix generate script

--- axiosTest/generate.js
+++ axiosTest/generate.js
@@ -2,14 +2,26 @@
 var fs = require('fs');
 var CodeGen = require('../lib/codegen').CodeGen;
 
-var file = "./axiosTest/invoice.json"
-
-var swagger = JSON.parse(fs.readFileSync(file, 'UTF-8').replace(/«/g,"").replace(/»/g,"").replace(/string,Link/g,"StringLink"));
+var invoice = JSON.parse(fs.readFileSync("./axiosTest/invoice.json", 'UTF-8').replace(/«/g,"").replace(/»/g,"").replace(/string,Link/g,"StringLink"));
+var kyc = JSON.parse(fs.readFileSync("./axiosTest/kyc.json", 'UTF-8').replace(/«/g,"").replace(/»/g,"").replace(/string,Link/g,"StringLink"));
 
 var tsSourceCode = CodeGen.getCustomCode({
     moduleName: 'Test',
     className: 'Test',
-    swagger: swagger,
+    swagger: invoice,
+    tslintFix: true,
+    beautify: false,
+    template: {
+        class: fs.readFileSync('templates/axios/class.mustache', 'utf-8'),
+        method: fs.readFileSync('templates/axios/method.mustache', 'utf-8'),
+        type: fs.readFileSync('templates/axios/type.mustache', 'utf-8')
+    }
+});
+
+var tsSourceCode2 = CodeGen.getCustomCode({
+    moduleName: 'Test',
+    className: 'Test',
+    swagger: kyc,
     tslintFix: true,
     beautify: false,
     template: {
@@ -20,7 +32,15 @@ var tsSourceCode = CodeGen.getCustomCode({
 });
 
 
+
 fs.writeFile("./axiosTest/invoice.ts", tsSourceCode, function(err) {
+    if (err) {
+        return console.log(err);
+    }
+    console.log("The file was saved!");
+});
+
+fs.writeFile("./axiosTest/kyc.ts", tsSourceCode2, function(err) {
     if (err) {
         return console.log(err);
     }
